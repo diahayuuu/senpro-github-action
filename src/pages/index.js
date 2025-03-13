6e05bbbc4ea2ea03737e8ab596836341969d4502
diahayuuu <diahayurahmawati@mail.ugm.ac.id>
2025-03-13 10:48:18 +0700

    update index

--- src/pages/index.js
+++ src/pages/index.js
@@ -30,7 +30,7 @@ export default function Home() {
 
         <div>
           <h5 className={inter.className}>
-            [Nama] | [NIU]
+            Diah | 499722
           </h5>
         </div>
       </main>
